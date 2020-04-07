c9e49a56a5d4549ce0cc882c951d54921c842054
NeoNuc2001 <privatekaoru0508@gmail.com>
2020-04-07 17:46:05 +0900

    Added some feature (Not Secured at all)

--- VirusMove.cpp
+++ VirusMove.cpp
@@ -14,17 +14,24 @@ uniform_int_distribution<> rand480(0, 480);
 class class_human{
 public:
   int Axis[2];
+  int speed[2];
+  int Acell[2];
   int InfectedFlag;
   int AlreadyInfectedFlag;
 public:
+  void setspeed(){if(speed[0]+Acell[0]>=-5&&speed[0]+Acell[0]<=5)speed[0]+=Acell[0];if(speed[1]+Acell[1]>=-5&&speed[1]+Acell[1]<=5)speed[1]+=Acell[1];};
+  void setAcell(){if(Axis[0]<=0)Acell[0]=rand()%3;else if(Axis[0]>=630)Acell[0]=rand()%3-2;else Acell[0]=rand()%3-1;if(Axis[1]<=0)Acell[1]=rand()%3; else if(Axis[1]>=470)Acell[1]=rand()%3-2;else Acell[1]=rand()%3-1;};
+  void setAxis(){Axis[0]+=speed[0];Axis[1]+=speed[1];};
+  void setAll(){setAcell();setspeed();setAxis();}
   class_human();
 };
 using namespace std;
 class_human::class_human(){
-
+  speed[0]=rand()%6-3;
+  speed[1]=rand()%6-3;
   Axis[0]=rand()%640;
   Axis[1]=rand()%480;
-  cout << "X:" << this->Axis[0] <<"Y:" << this->Axis[1];
+  //cout << "X:" << this->Axis[0] <<"Y:" << this->Axis[1];
   this->InfectedFlag=0;
   this->AlreadyInfectedFlag=0;
 }
@@ -32,10 +39,12 @@ class_human::class_human(){
 
 int main() {
 srand((unsigned int)time(NULL));
-  int PeopleNum;
+  int PeopleNum,PeopleElement;
   SetGraphMode( 640 , 480 , 16 );
   cout << "Set number of people\n";
   cin >> PeopleNum ;
+  cout << "Set Human to watch\n";
+  cin >>  PeopleElement;
   ChangeWindowMode(TRUE);
   	if( DxLib_Init() == -1 )		//
   	{
@@ -46,6 +55,7 @@ SetDrawScreen( DX_SCREEN_BACK );
   int humanhandle = LoadGraph("People.png");
   int setupflag=0;
   while( CheckHitKey( KEY_INPUT_ESCAPE ) == 0&&!ScreenFlip()&&!ProcessMessage()&&!ClearDrawScreen()){
+
     if(setupflag==0){
       for(int i=0;i<=PeopleNum;i++){
        human.push_back(class_human(/*rand()%640,rand()%480)*/));
@@ -55,14 +65,16 @@ SetDrawScreen( DX_SCREEN_BACK );
       setupflag=1;
 
     }else{
+      cout <<"SpeedX:" <<human[PeopleElement].speed[0] <<" SpeedY:" <<human[PeopleElement].speed[1] <<"\n AxisX" <<human[PeopleElement].Axis[0]<<" AxisY:" <<human[PeopleElement].Axis[1] <<"\n";
       #line 1
       ClearDrawScreen();
       for(int i=0;i<=PeopleNum-20;i++){
 
 
+        human[i].setAll();
 
-        DrawGraph(human[i].Axis[0] ,human[i].Axis[1],humanhandle, FALSE ) ;
-      //DrawPixel(human[i].Axis[0] ,human[i].Axis[1],GetColor(0,255,0));
+        //DrawGraph(human[i].Axis[0] ,human[i].Axis[1],humanhandle, FALSE ) ;
+      DrawPixel(human[i].Axis[0] ,human[i].Axis[1],GetColor(0,255,0));
 
 
       }
